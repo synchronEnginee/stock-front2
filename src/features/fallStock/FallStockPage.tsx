26e31539a6d7766402b8c34be30fdca37229db6b
synchronEnginee <te2.9niku.41cd@gmail.com>
2022-12-24 04:52:55 +0000

    chartjs upgrade for commonjs

--- src/features/fallStock/FallStockPage.tsx
+++ src/features/fallStock/FallStockPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { css } from '@emotion/react'
 import { Link } from 'react-router-dom'
+import FallStock from './components/FallStock'
 // import FallStock, { FallStockProps } from 'components/FallStock';
 // import DownloadExcelButton from 'components/DownloadExcelButton';
 // import axios, { AxiosError, AxiosResponse } from 'axios';
@@ -29,43 +30,11 @@ const styles = css({
 
 const FallStockPage = () => {
   // カスタムフック作る
-  const [pageIndex, setPageIndex] = useState(1)
+  // const [pageIndex, setPageIndex] = useState(1)
   // react-queryへ変更する. api.tsに切り出す
   //   const { data } = useSWR(`http://127.0.0.1:5000/${pageIndex}`, fetcher);
 
-  return (
-    <>
-      <Link to="/compare">銘柄比較へ</Link>
-      <table css={styles}>
-        <tr>
-          <th>銘柄名</th>
-          <th>銘柄コード</th>
-          <th>株価</th>
-          <th>下落率</th>
-          <th>目標株価</th>
-        </tr>
-        {/* {data &&
-          data.map((stock) => (
-            <FallStock
-              name={stock.name}
-              code={stock.code}
-              price={stock.price}
-              stockFall={stock.stockFall}
-              stockTargetPrice={stock.stockTargetPrice}
-            />
-          ))} */}
-      </table>
-      {!!(pageIndex >= 1) && (
-        <button type="button" onClick={() => setPageIndex(pageIndex - 1)}>
-          Previous
-        </button>
-      )}
-      <button type="button" onClick={() => setPageIndex(pageIndex + 1)}>
-        Next
-      </button>
-      {/* {data && <DownloadExcelButton data={data} />} */}
-    </>
-  )
+  return <FallStock />
 }
 
 export default FallStockPage
